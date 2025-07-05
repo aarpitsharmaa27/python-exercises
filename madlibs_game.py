86453a35f92641ab6775dfbdfc33ca5692cdc6c6
Arpit <arpit8894350818@gmail.com>
2025-07-05 13:25:37 +0530

    Add files via upload
     A word game where users input random words to build a silly story.

---
+++ madlibs_game.py
@@ -0,0 +1,22 @@
+
+    ## Madlibs Game
+    ## A word game where we create story
+
+adjective = input("Enter an adjective (e.g small, large, big etc.) :")
+noun1 = input("Enter a noun(name, place or thing) :")
+interjection = input("Enter an interjection(Sudden Reaction Wow!, Huh!, Alas! etc.) :")
+adjective2 = input("Enter an adjective (Description) :")
+verb = input("Enter a verb(ending with 'ing' :")
+
+verb1 = input("Enter a verb(ending with 'ing' ) :")
+
+
+
+print(f"Today I went to a {adjective} zoo.")
+print(f"In the zoo, I saw a {noun1}. ")
+print(f"{noun1} was very {adjective2} and {verb} there.")
+print(f"Then {noun1} suddenly saw me.")
+print(f"After seeing him, my reaction was {interjection}.")
+print(f"I don't know why but after that {noun1} said bye to me.")
+print(f"That visit to the {adjective} zoo and meeting the {noun1} really impressed me.")
+
